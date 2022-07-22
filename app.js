c2c46830c7261aaff3950e3afbc5bc5fd1e9ceab
LucasF <lucasfreitas286@outlook.com>
2022-07-22 00:05:01 -0300

    link API concertado

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ const botao_buscar = document.querySelector("#botao_buscar");
 const botao_limpar = document.querySelector("#botao_limpar");
 const apiKey = "d0f200f6";
 
-botao_buscar.addEventListener('click', ()=> { core(); });
+botao_buscar.addEventListener('click', ()=> { core(); limpaInput(); });
+window.addEventListener('keypress', (e)=> {if(e.key === "Enter"){ core(); limpaInput(); }});
+botao_limpar.addEventListener('click', () => { limpaInfo(); });
 
 async function buscaFilme(nomeBusca){
-    const resposta = await fetch(`http://www.omdbapi.com/?s=${nomeBusca}&apikey=${apiKey}`);
+    const resposta = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${nomeBusca}`);
     return resposta.json();
 }
 
@@ -24,8 +26,8 @@ async function buscaFilme(nomeBusca){
 async function core(){
     try{
         const filme = await buscaFilme(nomeBusca.value);
-        console.log(filme);
-        // validaFilme(filme);
+        validaFilme(filme);
+        defineValores(filme);
 
     }catch(erro){
         mensagemErro.textContent = erro;
@@ -33,5 +35,36 @@ async function core(){
 }
 
 function validaFilme(filme){
-    if(filme);
+    if(filme.Plot === undefined || filme.Year === undefined || filme.Actors === "N/A"){
+        throw new Error("Filme não encontrado!");
+    }
+}
+
+function defineValores(filme){
+    poster.setAttribute("src", filme.Poster);
+    titulo.textContent = filme.Title;
+    sinopse.textContent = filme.Plot;
+    ano.textContent = `Year: ${filme.Year}`;
+    duracao.textContent = `Runtime: ${filme.Runtime}`;
+    avaliacao.textContent = `Rating: ${filme.imdbRating}`;
+    genero.textContent = `Genre: ${filme.Genre}`;
+    atores.textContent = `Actors: ${filme.Actors}`;
+    diretor.textContent = `Director: ${filme.Director}`;
+}
+
+function limpaInfo(){
+    nomeBusca.value = "";
+    poster.setAttribute("src", "default_image.png");
+    titulo.textContent = '';
+    sinopse.textContent = '';
+    ano.textContent = '';
+    duracao.textContent = ''; 
+    avaliacao.textContent = '';
+    genero.textContent = '';
+    atores.textContent = '';
+    diretor.textContent = '';
+}
+
+function limpaInput(){
+    nomeBusca.value = '';
 }
